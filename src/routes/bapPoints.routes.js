8669147e23c3cd2f78a480c3234b37466ba8ae8b
Raymond Cox <raymondcdev@gmail.com>
2024-07-09 09:08:53 -0400

    add get / bap-points

--- src/routes/bapPoints.routes.js
+++ src/routes/bapPoints.routes.js
@@ -3,6 +3,10 @@ import { updatePoints } from '../controllers/bapPoints.controller.js'
 
 const router = Router()
 
+router.get('/', (_req, res) => {
+  res.send('Hello from the BAP Points route!')
+})
+
 router.post('/', async (req, res) => {
   const results = await updatePoints(req.body)
 
