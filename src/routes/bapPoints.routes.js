bef3decbe22d4baa031d8ed43a4c61584d639fba
Raymond Cox <raymondcdev@gmail.com>
2024-07-09 09:42:50 -0400

    updating route

--- src/routes/bapPoints.routes.js
+++ src/routes/bapPoints.routes.js
@@ -3,14 +3,14 @@ import { updatePoints } from '../controllers/bapPoints.controller.js'
 
 const router = Router()
 
-router.get('/', (_req, res) => {
-  res.send('Hello from the BAP Points route!')
-})
+router
+  .get('/', (_req, res) => {
+    res.send('Hello from the BAP Points route!')
+  })
+  .post('/add', async (req, res) => {
+    const results = await updatePoints(req.body)
 
-router.post('/', async (req, res) => {
-  const results = await updatePoints(req.body)
-
-  res.json(results)
-})
+    res.json(results)
+  })
 
 export default router
